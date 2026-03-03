6cff341c45cafa232bc8ca2a5924022682a9827e
epaulino27 <143731967+epaulino27@users.noreply.github.com>
2026-03-02 20:46:40 -0500

    Error System

--- src/routes/MainRouter.js
+++ src/routes/MainRouter.js
@@ -7,20 +7,23 @@ import SignUpPage from "../components/SignUpPage";
 import LoginPage from "../components/LoginPage";
 import { movieDataLoader } from "../loaders/loaders";
 import FallbackElement from "../components/FallbackElement";
+import ErrorPage, { ErrorBoundary } from "../components/ErrorPage";
 
 const MainRouter = [
   {
     path: "/",
     Component: Root,
+    ErrorBoundary: ErrorBoundary,
     children: [
       { index: true, Component: Home },
-      { path: "/dashboard",
+      { path: "dashboard",
         Component: Dashboard,
         loader: movieDataLoader,
         HydrateFallback: FallbackElement
       },
-      { path: "/signup", Component: SignUpPage },
-      { path: "/login", Component: LoginPage },
+      { path: "signup", Component: SignUpPage },
+      { path: "login", Component: LoginPage },
+      { path: "error", Component: ErrorPage },
     ],
   },
 ];
